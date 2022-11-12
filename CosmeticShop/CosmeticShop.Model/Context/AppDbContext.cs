086bf96ed04f376bd1a0b9c696b2bad58c8c9b95
Irada <salvivisal275@gmail.com>
2022-11-12 19:59:48 +0600

    added json and appDbContext

---
+++ CosmeticShop/CosmeticShop.Model/Context/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using CosmeticShop.Model.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+namespace StudentsManager.Backend.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            IConfiguration config = (IConfiguration)new ConfigurationBuilder()
+                .AddJsonFile("AppSettings.json")
+                .Build();
+
+            string connectionString = config.GetConnectionString("ConnectionString");
+
+            builder.UseSqlServer(connectionString);
+        }
+    }
+}
