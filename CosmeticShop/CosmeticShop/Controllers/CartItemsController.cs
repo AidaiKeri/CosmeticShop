43e4c8e7af772bf7090e24e44aa57e0b69803f38
Aidai <aidaikerimkulova@gmail.com>
2022-12-05 20:26:22 +0600

    Changed style

--- CosmeticShop/CosmeticShop/Controllers/CartItemsController.cs
+++ CosmeticShop/CosmeticShop/Controllers/CartItemsController.cs
@@ -29,20 +29,27 @@ namespace CosmeticShop.WebApp.Controllers
         {
             var user = _context.Users.FirstOrDefault();
             var product = _context.Product.Find(2);
-            var cartItem = new CartItem
+            try
             {
-                Product = (CartProduct)product,
-                User = (CartUser)user,
-                ProductId = product.Id,
-                UserId = user.Id,
-            };
+                var cartItem = new CartItem
+                {
+                    Product = (CartProduct)product,
+                    User = (CartUser)user,
+                    ProductId = product.Id,
+                    UserId = user.Id,
+                };
+                _repository.Add(cartItem);
+                var a = _repository.GetAll().ToList();
 
-            _repository.Add(cartItem);
-            var a = _repository.GetAll().ToList();
+                return NotFound();
+            }
+            catch(NullReferenceException ex)
+            {
+ 
+                return Problem(ex.Message);
 
-            return NotFound();
+            }
+            
         }
-
-        
     }
 }
